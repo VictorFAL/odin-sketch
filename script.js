7d8fe1f6c18f8811c09c03cd1b86ebcd0b4ea3b0
Victor | Harry <victorfalmeida12@gmail.com>
2022-05-11 10:47:26 -0300

    user can cutomize grid size

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 const canvas = document.getElementById('canvas');
 
+//fill canvas with divs
 for (let i = 1; i <= 16; i++) {
     for (let j = 1; j <= 16; j++) {
-    canvas.innerHTML += '<div class="pixel"></div>'
+    canvas.innerHTML += '<div class="pixel"></div>';
     }
 }
 
+
+// let user "draw" by hovering mouse
 let pixels = document.querySelectorAll('.pixel');
 
 pixels.forEach((pxl) => {
     pxl.addEventListener('mouseover', () => {
         pxl.style.backgroundColor = 'black';
+    });
+});
+
+
+// let user customize grid size
+const btnCustom = document.getElementById('btn-custom');
+
+btnCustom.addEventListener('click', () => {
+    let sqrNum = prompt('Insert the number of squares per side. (min: 2 | max: 100)');
+    
+    if (sqrNum < 2) {
+        sqrNum = 2;
+    } else if (sqrNum > 100) {
+        sqrNum = 100;
+    }
+
+    canvas.innerHTML = '';
+
+    for (let i = 1; i <= sqrNum; i++) {
+        for (let j = 1; j <= sqrNum; j++) {
+        canvas.innerHTML += '<div class="pixel"></div>';
+        }
+    }
+
+    canvas.style.gridTemplateColumns = `repeat(${sqrNum}, ${100 / sqrNum}%)`;
+    canvas.style.gridTemplateRows = `repeat(auto-fill, ${100 / sqrNum}%)`;
+
+    let pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach((pxl) => {
+        pxl.addEventListener('mouseover', () => {
+            pxl.style.backgroundColor = 'black';
+        });
     });
 });
